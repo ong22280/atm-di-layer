0c89d21528d09bd9b9fd953ba9204b684b711ba2
Sittipong Hemloun <77152671+ong22280@users.noreply.github.com>
2023-07-24 21:31:21 +0700

    spring IoC

--- src/main/java/atm/Bank.java
+++ src/main/java/atm/Bank.java
@@ -1,41 +1,48 @@
 package atm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 import java.util.Map;
 
 /**
  * A bank contains customers with bank accounts.
  */
+@Component
 public class Bank {
 
-   private String name;
-   private Map<Integer,Customer> customers;
-   private DataSource dataSource;
+    @Value("${bankname}")
+    private String name;
+    private Map<Integer, Customer> customers;
 
-   /**
-    * Constructs a bank with no customers.
-    */
-   public Bank(String name, DataSource dataSource) {
-      this.name = name;
-      this.dataSource = dataSource;
-      this.customers = dataSource.readCustomers();
-   }
+    @Autowired
+    private DataSource dataSource;
 
-   /**
-    * Adds a customer to the bank.
-    * @param customer the customer to add
-    */
-   public void registerCustomer(Customer customer) {
-      customers.put(customer.getId(), customer);
-   }
+    @PostConstruct
+    public void initCustomerData() {
+        this.customers = dataSource.readCustomers();
+    }
 
-   /**
-    * Finds a customer in the bank.
-    * @param id a customer id
-    * @return the matching customer, or null if no customer
-    * matches
-    */
-   public Customer findCustomer(int id) {
-      return customers.get(id);
-   }
+    /**
+     * Adds a customer to the bank.
+     *
+     * @param customer the customer to add
+     */
+    public void registerCustomer(Customer customer) {
+        customers.put(customer.getId(), customer);
+    }
+
+    /**
+     * Finds a customer in the bank.
+     *
+     * @param id a customer id
+     * @return the matching customer, or null if no customer
+     * matches
+     */
+    public Customer findCustomer(int id) {
+        return customers.get(id);
+    }
 }
 
